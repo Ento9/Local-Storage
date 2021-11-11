0d95350222592dd7b6cc2e79d8a06c00e92a1ae5
Ento9 <78752338+Ento9@users.noreply.github.com>
2021-11-11 18:44:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -22,3 +22,22 @@ for (let i = 0; i < localStorage.length; i++) {
   
   IsOutput.innerHTML += `${key}: ${value}<br />`;
 }
+
+document.querySelector("myFileInput").addEventListener("change", function () {
+  const reader = new FileReader();
+  
+  reader.addEventListener("load", () => {
+    
+    localStorage.setItem("recent-image", reader.result);
+  });
+  
+  reader.readAsDataURL(this.files[0]);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const recentImageDataUrl = localStorage.getItem("recent-image");
+  
+  if (recentImageDataUrl) {
+    document.querySelector("#imgPreview").setAttribute("src", recentImageDataUrl);
+  }
+});
